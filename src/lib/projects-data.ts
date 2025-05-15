3f7ca9632b630f190560bd7279130dfdc5128672
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-05-14 19:00:49 -0500

    feat: add dbob

--- src/lib/projects-data.ts
+++ src/lib/projects-data.ts
@@ -12,4 +12,12 @@ export const projects: Project[] = [
     linkedin: "https://www.linkedin.com/in/tomas-calle/",
     link: "#",
   },
+  {
+    name: "dbob",
+    description:
+      "Escalamos las ventas de los infoproductores con procesos de venta hiper-personalizado",
+    linkedin:
+      "https://www.linkedin.com/in/andre-portilla?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    link: "https://dbob.tech",
+  },
 ];
