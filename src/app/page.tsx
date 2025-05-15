d678f21a76a68b5a90119ba70acfa8e568b22994
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-05-14 19:18:44 -0500

    feat: hide projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <main className="flex h-full min-h-screen flex-col items-center justify-start bg-black text-white px-4 overflow-x-hidden py-12 md:py-16">
       <Info />
-      <Projects />
+      {/* <Projects /> */}
     </main>
   );
 }
